39eaf2ef70f5f1c1479bca05d4ed133a9cd5534c
madmath03 <mb.mathieu.brunot@gmail.com>
2021-03-28 23:53:21 +0000

    :wrench: Gitpod owner of openldap
    Signed-off-by: madmath03 <mb.mathieu.brunot@gmail.com>

--- .gitpod/bootstrap/slapd-init.sh
+++ .gitpod/bootstrap/slapd-init.sh
@@ -96,8 +96,8 @@ load_initial_data() {
 
 reconfigure_slapd
 make_snakeoil_certificate
-chown -R openldap:openldap /etc/ldap
-slapd -h "ldapi:///" -u openldap -g openldap
+chown -R gitpod:gitpod /etc/ldap
+slapd -h "ldapi:///" -u gitpod -g gitpod
 
 configure_msad_features
 configure_tls
